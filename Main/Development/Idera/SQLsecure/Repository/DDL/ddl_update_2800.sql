247cc581f2744514f9d3fbfd8aa56f0f90c0c636
ISemyanchuk <isemyan@softserveinc.com>
2014-11-12 11:29:12 +0200

    Encoding fixes

--- Main/Development/Idera/SQLsecure/Repository/DDL/ddl_update_2800.sql
+++ Main/Development/Idera/SQLsecure/Repository/DDL/ddl_update_2800.sql
@@ -22,24 +22,105 @@ BEGIN
 END
 ELSE
 BEGIN
- print 'test'
-END
+	/* ---------------------------------------------------------------------- */
+	/* Add Snapshot	Collection fields                                         */
+	/* ---------------------------------------------------------------------- */
+
+         BEGIN TRANSACTION
+
+         ALTER TABLE dbo.serversnapshot
+         ADD isdistributor NCHAR(1) NULL
+
+         ALTER TABLE dbo.serversnapshot
+         ADD ispublisher NCHAR(1) NULL
+
+         ALTER TABLE dbo.serversnapshot
+         ADD hasremotepublisher NCHAR(1) NULL
+
+         ALTER TABLE dbo.databaseobject
+         ADD PERMISSION_SET  INT NULL,
+		 CreateDate DATETIME NULL,
+		 ModifyDate DATETIME NULL
+
+         ALTER TABLE dbo.sqldatabase
+         ADD IsContained BIT null
+ 
+		ALTER TABLE dbo.serverosobject
+		ALTER COLUMN longname NVARCHAR(max) null
+
+		ALTER TABLE dbo.policyinterview
+		ALTER COLUMN interviewtext NVARCHAR(max) null
+   
+		ALTER TABLE dbo.assessment
+		ALTER COLUMN assessmentnotes NVARCHAR(max) not null
+
+		EXEC sp_refreshview N'[dbo].[vwregistrykey]'
+		EXEC sp_refreshview N'[dbo].[vwregistrykeypermission]'
+		EXEC sp_refreshview N'[dbo].[vwpolicymetric]'
+		EXEC sp_refreshview N'[dbo].[vwfilesystemobject]'
+		EXEC sp_refreshview N'[dbo].[vwfilesystemobjectpermission]'
+		EXEC sp_refreshview N'[dbo].[vwosobjectpermission]'
+		EXEC sp_refreshview N'[dbo].[vwpolicyassessment]'
+		EXEC sp_refreshview N'[dbo].[vwpolicychangelog]'
+		EXEC sp_refreshview N'[dbo].[vwpolicy]'    
+
+         IF NOT EXISTS ( SELECT *
+                         FROM   dbo.sysobjects
+                         WHERE  id = OBJECT_ID(N'[dbo].[sqljob]')
+                                AND xtype = N'U' ) 
+            BEGIN
+				
+                  CREATE TABLE [dbo].[sqljob]
+                         (
+                          [JobId] [int] IDENTITY(1 , 1) NOT NULL
+                         ,[SnapshotId] [int] NOT NULL
+                         ,[Name] [nvarchar](128) NOT NULL
+                         ,[Desciprion] [nvarchar](512) NULL
+                         ,[Step] [nvarchar](128) NULL
+                         ,[LastRunDate] [datetime] NULL
+                         ,[Command] [nvarchar](MAX) NULL
+                         ,[SubSystem] [nvarchar](40) NULL
+                         ,[Ownersid] [varbinary](85) NOT NULL
+                         ,[Enabled] [smallint] NULL
+                         )
+                  ON     [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 
 
-GO
+                  SET ANSI_PADDING OFF
 
-/* ---------------------------------------------------------------------- */
-/*	Updates to new fields must be done after finalizing the changes		  */
-/* ---------------------------------------------------------------------- */
-declare @ver int
-SELECT @ver=schemaversion FROM currentversion
-if (isnull(@ver,900) < 2800)
-BEGIN
-	BEGIN TRANSACTION
+                  ALTER TABLE [dbo].[sqljob]  WITH NOCHECK ADD  CONSTRAINT [FK_sqljob_serversnapshot] FOREIGN KEY([SnapshotId])
+                  REFERENCES [dbo].[serversnapshot] ([snapshotid])
 
-	--insert into the newly created configuration table
-	INSERT INTO dbo.configuration (lastupdated, isweakpassworddetectionenabled) VALUES (GETUTCDATE(), N'Y') 
-	
+                  ALTER TABLE [dbo].[sqljob] CHECK CONSTRAINT [FK_sqljob_serversnapshot]
+            END 
+         IF NOT EXISTS ( SELECT *
+                         FROM   dbo.sysobjects
+                         WHERE  id = OBJECT_ID(N'[dbo].[sqljobproxy]')
+                                AND xtype = N'U' ) 
+            BEGIN
+                  CREATE TABLE [dbo].[sqljobproxy]
+                         (
+                          [proxyId] [int] IDENTITY(1 , 1) NOT NULL
+                         ,[snapshotid] [int] NOT NULL
+                         ,[proxyName] [nvarchar](128) NOT NULL
+                         ,[enabled] [tinyint] NULL
+                         ,[usersid] [varbinary](85) NULL
+                         ,[subsystemid] [int] NULL
+                         ,[subsystem] [nvarchar](40) NULL
+                         ,[credentialId] [int] NULL
+                         ,[credentialName] [nvarchar](128) NULL
+                         ,[credentialIdentity] [nvarchar](128) NULL
+                         )
+                  ON     [PRIMARY]
+
+
+                  SET ANSI_PADDING OFF
+
+                  ALTER TABLE [dbo].[sqljobproxy]  WITH NOCHECK ADD  CONSTRAINT [FK_sqljobproxie_serversnapshot] FOREIGN KEY([snapshotid])
+                  REFERENCES [dbo].[serversnapshot] ([snapshotid])
+
+                  ALTER TABLE [dbo].[sqljobproxy] CHECK CONSTRAINT [FK_sqljobproxie_serversnapshot]
+            END 			       
 	COMMIT
 END
 
