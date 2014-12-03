f5112190e0ef402bc965df6b9881264f83752972
ISemyanchuk <isemyan@softserveinc.com>
2014-12-03 13:24:23 +0200

    fix

--- Main/Development/Idera/SQLsecure/Repository/DDL/update_2800_footer.sql
+++ Main/Development/Idera/SQLsecure/Repository/DDL/update_2800_footer.sql
@@ -1,6 +1,8 @@
-go
-
-print '11'
+declare @ver int
+SELECT @ver=schemaversion FROM currentversion
+if (isnull(@ver, 900) <= 2799)	-- Check to prevent this from running in future upgrades, but run for all prior versions at any version level because the version gets updated later
+BEGIN
+	print '1'
+END
 
 GO
-
