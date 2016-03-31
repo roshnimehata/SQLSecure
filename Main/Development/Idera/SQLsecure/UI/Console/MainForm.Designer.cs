adee7987b22ef51d76f69dbebd58e80cac3258f3
Yevgen Mazur <ymazur@softserveinc.com>
2016-03-31 07:57:16 -0700

    Changed minimal size of Main Form to prevent Report controls overlapping after form resize

--- Main/Development/Idera/SQLsecure/UI/Console/MainForm.Designer.cs
+++ Main/Development/Idera/SQLsecure/UI/Console/MainForm.Designer.cs
@@ -1621,7 +1621,7 @@ namespace Idera.SQLsecure.UI.Console
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoScroll = true;
-            this.ClientSize = new System.Drawing.Size(892, 666);
+            this.ClientSize = new System.Drawing.Size(1190, 666);
             this.Controls.Add(this.toolStripContainer1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MinimumSize = new System.Drawing.Size(800, 600);
