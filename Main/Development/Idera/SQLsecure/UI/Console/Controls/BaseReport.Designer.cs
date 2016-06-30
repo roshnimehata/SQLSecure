bc9fd2042377dbc1fc6d20e949be8c3f8f6a5f68
unknown <VM154421$@vm154421.projects.local>
2016-06-30 20:07:22 +0300

    VS project file version upgrade related changes

--- Main/Development/Idera/SQLsecure/UI/Console/Controls/BaseReport.Designer.cs
+++ Main/Development/Idera/SQLsecure/UI/Console/Controls/BaseReport.Designer.cs
@@ -161,7 +161,7 @@ namespace Idera.SQLsecure.UI.Console.Controls
             this._reportViewer.Visible = false;
 
             //next line replaced because of compilation error for studio 2013
-            this._reportViewer.Print += new ReportPrintEventHandler(this._reportViewer_Print);
+            this._reportViewer.Print += new System.ComponentModel.CancelEventHandler(this._reportViewer_Print);
             //this._reportViewer.Print += new Microsoft.Reporting.WinForms.ReportPrintEventHandler(this._reportViewer_Print);
 
             this._reportViewer.Hyperlink += new Microsoft.Reporting.WinForms.HyperlinkEventHandler(this._reportViewer_Hyperlink);
