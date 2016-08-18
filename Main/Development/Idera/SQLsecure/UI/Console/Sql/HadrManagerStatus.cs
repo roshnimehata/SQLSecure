c24b8f0eb182a09423e9e5077ae66911467ec49f
Ivan Semyanchuk <isemyan@softserveinc.com>
2016-08-18 08:28:15 -0700

    Added fix for AOG

---
+++ Main/Development/Idera/SQLsecure/UI/Console/Sql/HadrManagerStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace Idera.SQLsecure.UI.Console.SQL
+{
+    public enum HadrManagerStatus
+    {
+        NotStarted = 0,
+        StartedAndRunning,
+        NotStartedAndFailed,
+        NotApplicable = 999
+    }
+}
