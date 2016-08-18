33d38482ab048c0cc4230b1233870bced7c85b7b
sudhanshusipani <sipani4@gmail.com>
2016-08-18 10:08:20 -0500

    Update SQLServer.cs

--- Main/Development/Idera/SQLsecure/UI/Console/Sql/SQLServer.cs
+++ Main/Development/Idera/SQLsecure/UI/Console/Sql/SQLServer.cs
@@ -17,36 +17,7 @@ using Idera.SQLsecure.UI.Console.SQL;
 
 namespace Idera.SQLsecure.UI.Console.Sql
 {
-        public class SQLServerProperties
-    {
-        public HadrManagerStatus HadrManagerStatus { get; set; }
-        public string MachineName { get; set; }
-        public string ServerName { get; set; }
-        public string InstanceName { get; set; }
-        public string LocalNetAddress { get; set; }
-        public string ClientNetAddress { get; set; }
-        public string Version { get; set; }
-        public string HadrClusterName { get; set; }
-
-        public bool IsServerInAoag
-        {
-            get
-            {
-                return HadrManagerStatus == HadrManagerStatus.StartedAndRunning;
-            }
-        }
-
-        public SQLServerProperties()
-        {
-            MachineName = string.Empty;
-            ServerName = string.Empty;
-            InstanceName = string.Empty;
-            LocalNetAddress = string.Empty;
-            Version = string.Empty;
-            HadrClusterName = string.Empty;
-            HadrManagerStatus = HadrManagerStatus.NotApplicable;
-        }
-    }
+        
     /// <summary>
     /// Encapsulates unregistered SQL Server instance
     /// </summary>
