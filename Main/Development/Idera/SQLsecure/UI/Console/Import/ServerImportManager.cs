bcdd7dcffe2f0aa219db1cf671144c3b53882e89
Barkha <Barkha.khatri@accolitelabs.com>
2017-04-11 21:07:30 +0530

    Import issue while updating credentials

--- Main/Development/Idera/SQLsecure/UI/Console/Import/ServerImportManager.cs
+++ Main/Development/Idera/SQLsecure/UI/Console/Import/ServerImportManager.cs
@@ -193,7 +193,7 @@ namespace Idera.SQLsecure.UI.Console.Import
         {
             RegisteredServer.UpdateCredentials(repository.ConnectionString,
                 connectionName, itemToImport.UserName, itemToImport.Password,
-                itemToImport.AuthType == SqlServerAuthenticationType.WindowsAuthentication ? "W" : (itemToImport.AuthType == SqlServerAuthenticationType.SqlServerAuthentication ? "S" : "A"),
+                (itemToImport.AuthType == SqlServerAuthenticationType.WindowsAuthentication || itemToImport.AuthType == SqlServerAuthenticationType.SqlServerAuthentication) ? "W" : "S" ,
                 itemToImport.UseSameCredentials ? itemToImport.UserName : itemToImport.WindowsUserName,
                 itemToImport.UseSameCredentials
                     ? itemToImport.Password
