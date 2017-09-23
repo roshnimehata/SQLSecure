b8b8cc1a32087f88cf8640aa7f6a32bc5688ba91
Vikash <vikash.gupta@xorlabs.com>
2017-09-23 21:10:23 +0530

    SQLSECU-2001 and SQLSECU-2002 Fixed

--- Main/Development/Idera/SQLsecure/Repository/DDL/update_3110_footer.sql
+++ Main/Development/Idera/SQLsecure/Repository/DDL/update_3110_footer.sql
@@ -21,7 +21,50 @@ IF ( ISNULL(@ver, 900) < 3110 )
         
 		BEGIN
 			UPDATE dbo.metric
-			SET metricdescription = N'Determine whether row-level security is configured for specified tables on SQL Server 2016 or later' -- metricdescription - nvarchar(1024)
+			SET 
+				metricdescription = N'Determine whether row-level security is configured for specified tables on SQL Server 2016 or later', -- metricdescription - nvarchar(1024)
+				valuedescription = N'When enabled, this check will identify a risk if row-level security is not configured for specified tables on SQL Server 2016 or later. Please specify in [Server].[Database].[Schema].[Table] format.' -- metricdescription - nvarchar(1024)
+			WHERE metricid = @metricid
+		END;
+
+		IF EXISTS ( SELECT
+                            TOP 1 * 
+                        FROM
+                            policymetric
+                        WHERE
+                            metricid = @metricid )
+        
+		BEGIN
+			UPDATE dbo.policymetric
+			SET reporttext = N'Are specified tables using row-level security on SQL Server 2016 or later?'  -- reporttext - nvarchar(4000)
+			WHERE metricid = @metricid
+		END;
+
+		IF EXISTS ( SELECT
+                            TOP 1 * 
+                        FROM
+                            metricextendedinfo
+                        WHERE
+                            metricid = @metricid )
+        
+		BEGIN
+			UPDATE dbo.metricextendedinfo
+			SET 
+				metricdescription = N'Determine whether row-level security is configured for specified tables on Azure SQL Database' , -- metricdescription - nvarchar(1024)
+				valuedescription = N'When enabled, this check will identify a risk if row-level security is not configured for specified tables on Azure SQL Database. Please specify in [Server].[Database].[Schema].[Table] format.'  -- valuedescription - nvarchar(1024)
+			WHERE metricid = @metricid
+		END;
+
+		IF EXISTS ( SELECT
+                            TOP 1 * 
+                        FROM
+                            policymetricextendedinfo
+                        WHERE
+                            metricid = @metricid )
+        
+		BEGIN
+			UPDATE dbo.policymetricextendedinfo
+			SET reporttext = N'Are specified tables using row-level security on Azure SQL Database?'  -- reporttext - nvarchar(4000)
 			WHERE metricid = @metricid
 		END;
 
@@ -37,10 +80,53 @@ IF ( ISNULL(@ver, 900) < 3110 )
          
 		BEGIN
 			UPDATE dbo.metric
-			SET metricdescription = N'Determine whether dynamic data masking is configured for specified columns on SQL Server 2016 or later' -- metricdescription - nvarchar(1024)
+			SET 
+				metricdescription = N'Determine whether dynamic data masking is configured for specified columns on SQL Server 2016 or later', -- metricdescription - nvarchar(1024)
+				valuedescription = N'When enabled, this check will identify a risk if dynamic data masking is not configured for specified columns on SQL Server 2016 or later. Please specify in [Server].[Database].[Schema].[Table].[Column] format.' -- metricdescription - nvarchar(1024)
 			WHERE metricid = @metricid
 		END;
 		
+		IF EXISTS ( SELECT
+                            TOP 1 * 
+                        FROM
+                            policymetric
+                        WHERE
+                            metricid = @metricid )
+        
+		BEGIN
+			UPDATE dbo.policymetric
+			SET reporttext = N'Are specified columns using dynamic data masking on SQL Server 2016 or later?' -- reporttext - nvarchar(4000)
+			WHERE metricid = @metricid
+		END;
+
+		IF EXISTS ( SELECT
+                            TOP 1 * 
+                        FROM
+                            metricextendedinfo
+                        WHERE
+                            metricid = @metricid )
+        
+		BEGIN
+			UPDATE dbo.metricextendedinfo
+			SET 
+				metricdescription = N'Determine whether dynamic data masking is configured for specified columns on Azure SQL Database' , -- metricdescription - nvarchar(1024)
+				valuedescription = N'When enabled, this check will identify a risk if dynamic data masking is not configured for specified columns on Azure SQL Database. Please specify in [Server].[Database].[Schema].[Table].[Column] format.' -- metricdescription - nvarchar(1024)
+			WHERE metricid = @metricid
+		END;
+
+		IF EXISTS ( SELECT
+                            TOP 1 * 
+                        FROM
+                            policymetricextendedinfo
+                        WHERE
+                            metricid = @metricid )
+        
+		BEGIN
+			UPDATE dbo.policymetricextendedinfo
+			SET reporttext = N'Are specified columns using dynamic data masking on Azure SQL Database?'  -- reporttext - nvarchar(4000)
+			WHERE metricid = @metricid
+		END;
+
 -----Unauthorized Accounts Check
 		SELECT
             @metricid = 71;
